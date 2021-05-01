3b4b319bee755b31a3ff1cc09d4f109ab9d048e4
njorojohn <johnnjoroge40@gmail.com>
2021-05-01 17:57:09 +0300

    chnged files from main to note

--- app/src/main/java/com/njoro/noteskeeper/NoteActivity.java
+++ app/src/main/java/com/njoro/noteskeeper/NoteActivity.java
@@ -35,7 +35,7 @@ public class NoteActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_note, menu);
         return true;
     }
 
