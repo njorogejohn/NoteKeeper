ed26fff143cf02aa9b4d920e1c80cdf1ca65868f
njorojohn <johnnjoroge40@gmail.com>
2021-05-02 20:16:47 +0300

    made noteinfo parcelable

--- app/src/main/java/com/njoro/noteskeeper/NoteInfo.java
+++ app/src/main/java/com/njoro/noteskeeper/NoteInfo.java
@@ -3,7 +3,7 @@ package com.njoro.noteskeeper;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public final class NoteInfo {
+public final class NoteInfo implements Parcelable {
     private CourseInfo mCourse;
     private String mTitle;
     private String mText;
@@ -13,6 +13,24 @@ public final class NoteInfo {
         mTitle = title;
         mText = text;
     }
+
+    protected NoteInfo(Parcel in) {
+        mCourse = in.readParcelable(CourseInfo.class.getClassLoader());
+        mTitle = in.readString();
+        mText = in.readString();
+    }
+
+    public static final Creator<NoteInfo> CREATOR = new Creator<NoteInfo>() {
+        @Override
+        public NoteInfo createFromParcel(Parcel in) {
+            return new NoteInfo(in);
+        }
+
+        @Override
+        public NoteInfo[] newArray(int size) {
+            return new NoteInfo[size];
+        }
+    };
 
     public CourseInfo getCourse() {
         return mCourse;
@@ -62,4 +80,15 @@ public final class NoteInfo {
         return getCompareKey();
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable(mCourse, 0);
+        dest.writeString(mTitle);
+        dest.writeString(mText);
+    }
 }
